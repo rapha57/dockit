38ffd2e5842e99a2a4b248aeaeadb2a0773b5a96
Raphael B <64146746+rapha57@users.noreply.github.com>
2026-09-09 21:27:34 +0200

    Revert: restore original icons (LayoutGrid + Menu), remove bootstrap-icons

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -42,6 +42,7 @@ import {
   Lock,
   LogIn,
   LogOut,
+  Menu,
   Minus,
   MoreHorizontal,
   MousePointerClick,
@@ -198,22 +199,6 @@ import type {
 } from "@/lib/portal";
 import type { Category, CategoryMoveImpact } from "@/lib/acl";
 import type { Directory } from "@/lib/ldap-runtime";
-
-function MenuDownIcon({ className }: { className?: string }) {
-  return (
-    <svg className={className} xmlns="http://www.w3.org/2000/svg" viewBox="1 1 14 14">
-      <path fill="currentColor" d="M0 1.5A1.5 1.5 0 0 1 1.5 0h2A1.5 1.5 0 0 1 5 1.5v2A1.5 1.5 0 0 1 3.5 5h-2A1.5 1.5 0 0 1 0 3.5zM1.5 1a.5.5 0 0 0-.5.5v2a.5.5 0 0 0 .5.5h2a.5.5 0 0 0 .5-.5v-2a.5.5 0 0 0-.5-.5zM0 8a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm1 3v2a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2zm14-1V8a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v2zM2 8.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m0 4a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5" />
-    </svg>
-  );
-}
-
-function MenuAppIcon({ className }: { className?: string }) {
-  return (
-    <svg className={className} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-      <path d="M0 1.5A1.5 1.5 0 0 1 1.5 0h2A1.5 1.5 0 0 1 5 1.5v2A1.5 1.5 0 0 1 3.5 5h-2A1.5 1.5 0 0 1 0 3.5zM1.5 1a.5.5 0 0 0-.5.5v2a.5.5 0 0 0 .5.5h2a.5.5 0 0 0 .5-.5v-2a.5.5 0 0 0-.5-.5zM0 8a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm1 3v2a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2zm14-1V8a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v2zM2 8.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m0 4a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5" />
-    </svg>
-  );
-}
 
 export const Route = createFileRoute("/")({
   loader: async () => {
@@ -5483,7 +5468,7 @@ function AppCard({
       onPointerDown={(e) => e.stopPropagation()}
     >
       {" "}
-      {app.linkMenu ? <MenuDownIcon className="size-3.5" /> : <MenuAppIcon className="size-3.5" />}
+      {app.linkMenu ? <LayoutGrid className="size-3.5" /> : <Menu className="size-3.5" />}
     </button>
   ) : null;
   const corner =
